cab98e3f29abac09768fe7cb8c70865c623881d3
Robert Strandh <robert.strandh@gmail.com>
2025-01-20 14:06:17 +0100

    Export names of functions.

--- simulated-floating-point/packages.lisp
+++ simulated-floating-point/packages.lisp
@@ -2,4 +2,18 @@
 
 (defpackage #:buoy-simulate
   (:use #:common-lisp)
-  (:export))
+  (:export
+   #:single-float-from-components
+   #:single-float-from-rational
+   #:rational-from-single-float
+   #:single-float-binary-+
+   #:single-float-binary--
+   #:single-float-binary-*
+   #:single-float-binary-/
+   #:double-float-from-components
+   #:double-float-from-rational
+   #:rational-from-double-float
+   #:double-float-binary-+
+   #:double-float-binary--
+   #:double-float-binary-*
+   #:double-float-binary-/))
