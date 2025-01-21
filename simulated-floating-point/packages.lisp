7d0bb4d8472f8bfc71d0193f00221c5b8119e7c8
Robert Strandh <robert.strandh@gmail.com>
2025-01-21 17:09:21 +0100

    Shadow names of floating-point constants.

--- simulated-floating-point/packages.lisp
+++ simulated-floating-point/packages.lisp
@@ -2,6 +2,19 @@
 
 (defpackage #:buoy-simulate
   (:use #:common-lisp)
+  (:shadow
+   #:most-positive-single-float
+   #:most-negative-single-float
+   #:least-positive-single-float
+   #:least-positive-normalized-single-float
+   #:least-negative-single-float
+   #:least-negative-normalized-single-float
+   #:most-positive-double-float
+   #:most-negative-double-float
+   #:least-positive-double-float
+   #:least-positive-normalized-double-float
+   #:least-negative-double-float
+   #:least-negative-normalized-double-float)
   (:export
    #:single-float-from-components
    #:single-float-from-rational
