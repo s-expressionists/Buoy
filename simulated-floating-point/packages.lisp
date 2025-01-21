00a74d06c63cffd778be1a1c366daa5380796b22
Robert Strandh <robert.strandh@gmail.com>
2025-01-21 17:18:04 +0100

    Export shadowed symbols.

--- simulated-floating-point/packages.lisp
+++ simulated-floating-point/packages.lisp
@@ -3,18 +3,18 @@
 (defpackage #:buoy-simulate
   (:use #:common-lisp)
   (:shadow
-   #:most-positive-single-float
-   #:most-negative-single-float
-   #:least-positive-single-float
-   #:least-positive-normalized-single-float
-   #:least-negative-single-float
-   #:least-negative-normalized-single-float
-   #:most-positive-double-float
-   #:most-negative-double-float
-   #:least-positive-double-float
-   #:least-positive-normalized-double-float
-   #:least-negative-double-float
-   #:least-negative-normalized-double-float)
+   . #1=(#:most-positive-single-float
+         #:most-negative-single-float
+         #:least-positive-single-float
+         #:least-positive-normalized-single-float
+         #:least-negative-single-float
+         #:least-negative-normalized-single-float
+         #:most-positive-double-float
+         #:most-negative-double-float
+         #:least-positive-double-float
+         #:least-positive-normalized-double-float
+         #:least-negative-double-float
+         #:least-negative-normalized-double-float))
   (:export
    #:single-float-from-components
    #:single-float-from-rational
@@ -29,4 +29,7 @@
    #:double-float-binary-+
    #:double-float-binary--
    #:double-float-binary-*
-   #:double-float-binary-/))
+   #:double-float-binary-/
+   #:integer-decode-single-float
+   #:integer-decode-double-float
+   . #1#))
