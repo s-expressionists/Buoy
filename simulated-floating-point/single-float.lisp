d01f9d625f0933cb88dffcd55625eba5f4e12d17
Robert Strandh <robert.strandh@gmail.com>
2025-01-22 04:28:42 +0100

    Define constant MOST-NEGATIVE-SINGLE-FLOAT.

--- simulated-floating-point/single-float.lisp
+++ simulated-floating-point/single-float.lisp
@@ -48,6 +48,9 @@
 (defconstant most-positive-single-float
   (single-float-from-components 0 254 (1- (expt 2 23))))
 
+(defconstant most-negative-single-float
+  (single-float-from-components 1 254 (1- (expt 2 23))))
+
 (defun integer-decode-single-float (single-float)
   (values (+ (ldb (byte 23 0) single-float) (expt 2 23))
           (- (ldb (byte 8 23) single-float) 127 23)
