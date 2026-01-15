c971604a50eaaf7fb39d7774374897681dca613b
Robert Strandh <robert.strandh@gmail.com>
2026-01-15 07:17:01 +0100

    Define function RATIONAL-CAN-BE-NORMAL-BINARY32.

--- simulated-floating-point/binary32.lisp
+++ simulated-floating-point/binary32.lisp
@@ -8,6 +8,13 @@
 (defconstant ratio-must-be-greater-for-normal-binary32
   (- (ash 1 127)))
 
+;;; Return true if and only if the argument can be rounded so that it
+;;; can be represented as a binary32 float.
+(defun rational-can-be-normal-binary32 (rational)
+  (< ratio-must-be-less-for-normal-binary32
+     rational 
+     ratio-must-be-greater-for-normal-binary32))
+
 (defclass binary32 ()
   ())
 
