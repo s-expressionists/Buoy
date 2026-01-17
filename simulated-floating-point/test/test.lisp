e7804fdb0bc768d5ec87e14c4a63d2a9d57ffa7a
Robert Strandh <robert.strandh@gmail.com>
2026-01-17 20:38:21 +0100

    Define function NORMAL-COMPONENTS-TO-FLOATR.

--- simulated-floating-point/test/test.lisp
+++ simulated-floating-point/test/test.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:buoy-simulate-test)
+
+(defun normal-components-to-floatr (exponent stored-mantissa)
+  (let ((real-mantissa (1+ (/ stored-mantissa (ash 1 23)))))
+    (if (minusp exponent)
+        (/ real-mantissa (ash 1 (- exponent)))
+        (* real-mantissa (ash 1 exponent)))))
