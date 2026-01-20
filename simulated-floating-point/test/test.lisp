cbd520de8957e5989b827438b4094f6a3e83688d
Robert Strandh <robert.strandh@gmail.com>
2026-01-20 14:25:43 +0100

    Define function TEST-NORMAL-EVEN-ODD.

--- simulated-floating-point/test/test.lisp
+++ simulated-floating-point/test/test.lisp
@@ -17,5 +17,13 @@
     (assert (= result1 upper))
     (assert (= result2 upper))
     (assert (= result3 lower))))
-    
-  
+
+(defun test-normal-even-odd (lower upper)
+  (let* ((average (/ (+ lower upper) 2))
+         (result1 (buoy:binary-32-from-rational average))
+         (result2 (buoy:binary-32-from-rational (+ average +epsilon+)))
+         (result3 (buoy:binary-32-from-rational (- average +epsilon+))))
+    ;; It should round to even
+    (assert (= result1 lower))
+    (assert (= result2 upper))
+    (assert (= result3 lower))))
