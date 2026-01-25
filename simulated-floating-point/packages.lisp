cdba856ecaed9dcc29d62ede781234c4238cd1e7
Robert Strandh <robert.strandh@gmail.com>
2026-01-25 14:31:38 +0100

    Update package definitiion for new representation.

--- simulated-floating-point/packages.lisp
+++ simulated-floating-point/packages.lisp
@@ -2,36 +2,15 @@
 
 (defpackage #:buoy-simulate
   (:use #:common-lisp)
-  (:shadow
-   . #1=(#:most-positive-single-float
-         #:most-negative-single-float
-         #:least-positive-single-float
-         #:least-positive-normalized-single-float
-         #:least-negative-single-float
-         #:least-negative-normalized-single-float
-         #:most-positive-double-float
-         #:most-negative-double-float
-         #:least-positive-double-float
-         #:least-positive-normalized-double-float
-         #:least-negative-double-float
-         #:least-negative-normalized-double-float))
   (:export
    #:binary32-from-rational
    #:value
-   #:single-float-from-components
-   #:single-float-from-rational
-   #:rational-from-single-float
    #:binary32-+
    #:binary32--
    #:binary32-*
    #:binary32-/
-   #:double-float-from-components
-   #:double-float-from-rational
-   #:rational-from-double-float
-   #:double-float-binary-+
-   #:double-float-binary--
-   #:double-float-binary-*
-   #:double-float-binary-/
-   #:integer-decode-single-float
-   #:integer-decode-double-float
-   . #1#))
+   #:binary64-from-rational
+   #:binary64-+
+   #:binary64--
+   #:binary64-*
+   #:binary64-/))
