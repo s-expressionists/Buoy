cb9ce98ee11581530623a89086a6c41f17beb539
Robert Strandh <robert.strandh@gmail.com>
2026-03-01 13:34:33 +0100

    Add comment for subnomal floatr.

--- simulated-floating-point/integer-decode-floatr.lisp
+++ simulated-floating-point/integer-decode-floatr.lisp
@@ -6,6 +6,13 @@
       (let ((numerator (numerator floatr))
             (denominator (denominator floatr)))
         (if (< floatr (/ (ash 1 126)))
+            ;; For subnormal floatrs, we always want to return -149 as
+            ;; the second value, because -149 = -126 - 23 where 23 is
+            ;; the number of bits in the mantissa.  So we need to
+            ;; figure out the exponent represented by the denominator,
+            ;; which is the INTEGER-LENGTH of the denominator (call it
+            ;; L) minus 1.  So we need to shift the denominator left
+            ;; by 149 - (L-1) = 150 - L.
             (let* ((length (integer-length denominator))
                    (diff (- 150 length)))
               (values (ash numerator diff) -149))
