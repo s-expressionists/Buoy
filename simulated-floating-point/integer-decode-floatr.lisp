7ef15b01250df4ae5a99bca6b4d5bbb557b59273
Robert Strandh <robert.strandh@gmail.com>
2026-03-01 14:22:22 +0100

    Define function INTEGER-DECODE-DOUBLE-FLOATR.

--- simulated-floating-point/integer-decode-floatr.lisp
+++ simulated-floating-point/integer-decode-floatr.lisp
@@ -41,3 +41,45 @@
                   ;; which is 1 - L - diff.
                   (values shifted
                           (- 1 (integer-length denominator) diff))))))))
+
+(defun integer-decode-double-floatr (floatr)
+  (if (zerop floatr)
+      (values 0 0)
+      (let ((numerator (numerator floatr))
+            (denominator (denominator floatr)))
+        (if (< floatr (/ (ash 1 1022)))
+            ;; For subnormal floatrs, we always want to return -1074
+            ;; as the second value, because -1074 = -1022 - 52 where
+            ;; 52 is the number of bits in the mantissa.  So we need
+            ;; to figure out the exponent represented by the
+            ;; denominator, which is the INTEGER-LENGTH of the
+            ;; denominator (call it L) minus 1.  So we need to shift
+            ;; the denominator left by 1074 - (L-1) = 1075 - L.
+            (let* ((length (integer-length denominator))
+                   (diff (- 1075 length)))
+              (values (ash numerator diff) -1074))
+            ;; For normal floatrs, we always want to return a first
+            ;; value with an INTEGER-LENGTH of 53, which is the 52
+            ;; bits of the represented mantissa plus the implicit 1.
+            (let* ((length (integer-length numerator))
+                   (diff (- 53 length))
+                   (shifted (ash numerator diff)))
+              (if (minusp diff)
+                  ;; When the difference is negative, this means that
+                  ;; the INTEGER-LENGTH of the numerator is greater
+                  ;; than 53, so we have a large integer and the
+                  ;; denominator is then 1.  So we return the shifted
+                  ;; numerator and the negative value of the diff.
+                  (values shifted
+                          (- diff))
+                  ;; when the difference is non-negative, we have a
+                  ;; floatr with a value that has a denominator that
+                  ;; is greater than or equal to 1.  The exponent
+                  ;; represented by the denominator is again (L-1)
+                  ;; where L is the INTEGER-LENGTH of the denominator.
+                  ;; Since we shifted the numerator left (i.e., we
+                  ;; made it greater) we need to make the denominator
+                  ;; greater as well, so we return -((L-1) + diff)
+                  ;; which is 1 - L - diff.
+                  (values shifted
+                          (- 1 (integer-length denominator) diff))))))))
