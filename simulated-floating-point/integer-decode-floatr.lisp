b835a16925b12a8232ae07d5b4be33db291a6947
Robert Strandh <robert.strandh@gmail.com>
2026-03-01 13:12:55 +0100

    Rewrite to decrease scopes of variables.

--- simulated-floating-point/integer-decode-floatr.lisp
+++ simulated-floating-point/integer-decode-floatr.lisp
@@ -1,20 +1,19 @@
 (cl:in-package #:buoy-simulate)
 
 (defun integer-decode-single-floatr (floatr)
-  (let* ((numerator (numerator floatr))
-         (denominator (denominator floatr))
-         (length (integer-length numerator))
-         (diff (- 24 length))
-         (shifted (ash numerator diff)))
-    (cond ((zerop floatr)
-           (values 0 0))
-          ((< floatr (/ (ash 1 126)))
-           (let* ((length (integer-length denominator))
-                  (diff (- 150 length)))
-             (values (ash numerator diff) -149)))
-          ((minusp diff)
-           (values shifted
-                   (- diff)))
-          (t
-           (values shifted
-                   (- 1 (integer-length (denominator floatr)) diff))))))
+  (if (zerop floatr)
+      (values 0 0)
+      (let ((numerator (numerator floatr))
+            (denominator (denominator floatr)))
+        (if (< floatr (/ (ash 1 126)))
+            (let* ((length (integer-length denominator))
+                   (diff (- 150 length)))
+              (values (ash numerator diff) -149))
+            (let* ((length (integer-length numerator))
+                   (diff (- 24 length))
+                   (shifted (ash numerator diff)))
+              (if (minusp diff)
+                  (values shifted
+                          (- diff))
+                  (values shifted
+                          (- 1 (integer-length denominator) diff))))))))
