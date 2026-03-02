6511553669bb19d52e1bea23258834baddd3c927
Robert Strandh <robert.strandh@gmail.com>
2026-03-02 16:25:43 +0100

    Define function LEAST-POSITIVE-NORMAL-EXPONENT.

--- simulated-floating-point/integer-decode-floatr.lisp
+++ simulated-floating-point/integer-decode-floatr.lisp
@@ -1,5 +1,8 @@
 (cl:in-package #:buoy-simulate)
 
+(defun least-positive-normal-exponent (exponent-width)
+  (- 2 (ash 1 (1- exponent-width))))
+
 (defun least-positive-normal-floatr (exponent-width)
   (/ (ash 1 (- (ash 1 (1- exponent-width)) 2))))
 
