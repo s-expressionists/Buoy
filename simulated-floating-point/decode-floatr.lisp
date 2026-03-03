c3ca76b6d74020745f8d3e7b67d0521ce3564ca9
Robert Strandh <robert.strandh@gmail.com>
2026-03-03 13:01:15 +0100

    Simplify DECODE-FLOATR.

--- simulated-floating-point/decode-floatr.lisp
+++ simulated-floating-point/decode-floatr.lisp
@@ -5,16 +5,9 @@
         (denominator (denominator floatr)))
     (let ((numerator-length (integer-length numerator))
           (denominator-length (integer-length denominator)))
-      (let ((difference (- numerator-length denominator-length)))
+      (let* ((difference (- numerator-length denominator-length))
+             (shift (1+ difference)))
         (if (minusp difference)
-            (setf numerator (ash numerator (- difference)))
-            (setf denominator (ash denominator difference)))
-        ;; We need to return a first value that is between 0.5
-        ;; (inclusive) and 1 (exclusive).  If the numerator is
-        ;; strictly less than the denominator, then the numerator and
-        ;; the denominator already designates such a number.
-        ;; Otherwise, we must increase the value of the denominator.
-        (unless (< numerator denominator)
-          (setf denominator (ash denominator 1))
-          (incf difference))
-        (values (/ numerator denominator) difference)))))
+            (setf numerator (ash numerator (- shift)))
+            (setf denominator (ash denominator shift)))
+        (values (/ numerator denominator) shift)))))
