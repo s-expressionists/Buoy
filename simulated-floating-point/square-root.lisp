804d59b47ecb36f1940cf1f9fc4500030ab1e89e
Robert Strandh <robert.strandh@gmail.com>
2026-03-11 05:55:33 +0100

    Define function RATIONAL-SQUARE-ROOT.

--- simulated-floating-point/square-root.lisp
+++ simulated-floating-point/square-root.lisp
@@ -32,3 +32,42 @@
 ;;; precision of quadruple-precision IEEE floating point.
 (defparameter *iteration-count*
   (iteration-count (/ (ash 1 120))))
+
+;;; We use the equivalence between (sqrt x) and
+;;; (* (expt 2 (- n)) (sqrt (* x (expt 2 (* 2 n))))) to scale the 
+;;; argument so that it is between 1 (inclusive) and 4 (exclusive).
+;;; That way, we are sure that *ITERATION-COUNT* is enough to get
+;;; a result with the desired precision.
+(defun rational-square-root (rational)
+  (let* ((numerator (numerator rational))
+         (numerator-length (integer-length numerator))
+         (denominator (denominator rational))
+         (denominator-length (integer-length denominator))
+         (diff (- numerator-length denominator-length)))
+    (if (minusp diff)
+        ;; We need to increase the numerator.
+        (setf numerator (ash numerator (- diff)))
+        ;; We need to increase the denominator.
+        (setf denominator (ash denominator diff)))
+    (if (< numerator denominator)
+        ;; We need to shift the numerator by 1 or 2 steps so that DIFF
+        ;; is even.
+        (if (evenp diff)
+            (progn (setf numerator (ash numerator 2))
+                   (decf diff 2))
+            (progn (setf numerator (ash numerator 1))
+                   (decf diff 1)))
+        ;; Numerator is greater than or equal to the denominator, but
+        ;; it is possible that DIFF is odd, and in that case we need
+        ;; to shift the numerator by one step.
+        (when (oddp diff)
+          (setf numerator (ash numerator 1))
+          (decf diff 1)))
+    ;; Now (/ numerator denominator) is a number of the right
+    ;; magnitude. So we copute the square root of it.
+    (let* ((result (newton (/ numerator denominator) *iteration-count*))
+           (result-numerator (numerator result))
+           (result-denominator (denominator result)))
+      (if (minusp diff)
+          (/ result-numerator (ash result-denominator (- (/ diff 2))))
+          (/ (ash result-numerator (/ diff 2)) result-denominator)))))
