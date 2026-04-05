66860334e7bb17e915ad53f0c300f1f43c04a27a
Robert Strandh <robert.strandh@gmail.com>
2026-04-05 16:03:49 +0200

    Define variable *LOG2*.

--- src/64/sin/custom-float.lisp
+++ src/64/sin/custom-float.lisp
@@ -24,3 +24,22 @@
    :low #x0
    :exponent 0
    :sign 1))
+
+(defparameter *e-rational*
+  (let ((e-string "2718281828459045235360287471352662497757247093699959574966"))
+    (/ (read-from-string e-string)
+       (expt 10 (1- (length e-string))))))
+
+(defparameter *log2*
+  (let* ((stuff (- 1 (/ 2 *e-rational*)))
+         (log2-rational
+           (- 1 (loop for i from 1 to 128
+                      for numerator = stuff then (* numerator stuff)
+                      sum (/ numerator i))))
+         (bits (round (* (expt 2 128) log2-rational))))
+    (make-custom-float-64
+     :high (ldb (byte 64 64) bits)
+     :low (ldb (byte 64 0) bits)
+     :exponent -1
+     :sign 0)))
+     
