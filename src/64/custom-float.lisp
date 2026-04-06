1e2705af9427320a008b902ab9fdc997b5bc3359
Robert Strandh <robert.strandh@gmail.com>
2026-04-06 10:20:09 +0200

    Provide type declaration for arguments.

--- src/64/custom-float.lisp
+++ src/64/custom-float.lisp
@@ -70,6 +70,7 @@
 ;;; memory.  The hope is that the destination will be supplied by the
 ;;; caller, and that it can be considered as having dynamic extent.
 (defun copy-custom-float-64 (destination source)
+  (declare (type custom-float-64 destination source))
   (setf (low destination) (low source)
         (high destination) (high source)
         (exponent destination) (exponent source)
