2a29c872e903c1107a498645028e6fcc5f96b607
Robert Strandh <robert.strandh@gmail.com>
2026-04-07 08:11:52 +0200

    Define A-MULTIPLY, S-MULTIPLY, and D-MULTIPLY.

--- src/64/utilities.lisp
+++ src/64/utilities.lisp
@@ -20,3 +20,31 @@
   (float (+ (* (rational x) (rational y))
             (rational z))
          1d0))
+
+;;; Multiply exactly A and B such that the sum of HIGH and LOW is the
+;;; exact product of A and B.  The core-math library call this
+;;; function `a_mul' and I have no ideas for a much improved name.
+(defun a-multiply (a b)
+  (declare (type double-float a b))
+  (let* ((high (* a b))
+         (low (fma a b (- high))))
+    (values high low)))
+
+;;; Multiply a by the sum of b-high and b-low.
+(defun s-multiply (a b-high b-low)
+  (declare (type double-float a b-high b-low))
+  (multiple-value-bind (high low) (a-multiply a b-high) ; exact
+    (values high (fma a b-low low))))
+
+;;; Compute (- (* (+ a-high a-low) (b-high b-low)) (* a-low b-low))
+;;; This code is a direct translation of the C code in the core-math
+;;; library.  The comment in that library says that we can ignore the
+;;; product of a-low and b-low if we assume that a-low is less than or
+;;; equal to ulp(a-high) and b-low is less than or equal to
+;;; ulp(b-high).
+(defun d-multiply (a-high a-low b-high b-low)
+  (declare (type double-float a-high a-low b-high b-low))
+  (multiple-value-bind (high s) (a-multiply a-high b-high)
+    (let* ((tt (fma a-low b-high s))
+           (low (fma a-high b-low tt)))
+      (values high low))))
