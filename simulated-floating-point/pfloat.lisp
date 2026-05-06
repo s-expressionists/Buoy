916761e5e6d696356e5d9dddfa0789549950e1ed
Robert Strandh <robert.strandh@gmail.com>
2026-05-06 14:09:42 +0200

    Define MULTIPLY-PFLOAT and RATIONAL-FROM-PFLOAT.

--- simulated-floating-point/pfloat.lisp
+++ simulated-floating-point/pfloat.lisp
@@ -33,3 +33,16 @@
 (defun make-pfloat (mantissa exponent)
   (normalize-pfloat (cons mantissa exponent)))
   
+(defun multiply-pfloat (pfloat1 pfloat2)
+  (let ((mantissa (* (pfloat-mantissa pfloat1)
+                     (pfloat-mantissa pfloat2)))
+        (exponent (+ (pfloat-exponent pfloat1)
+                     (pfloat-exponent pfloat2))))
+    (normalize-pfloat (make-pfloat mantissa exponent))))
+
+(defun rational-from-pfloat (pfloat)
+  (* (pfloat-mantissa pfloat)
+     (expt 2 (pfloat-exponent pfloat))))
+
+(defun double-float-from-pfloat (pfloat)
+  (dfloat (rational-from-pfloat pfloat)))
