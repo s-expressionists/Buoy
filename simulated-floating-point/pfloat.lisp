16ab745098156ac66ee3a2b222dd00b3c4887d5b
Robert Strandh <robert.strandh@gmail.com>
2026-05-09 09:25:29 +0200

    Add CL: package prefix where required.

--- simulated-floating-point/pfloat.lisp
+++ simulated-floating-point/pfloat.lisp
@@ -25,7 +25,7 @@
 (defun normalize-pfloat (pfloat)
   (let* ((mantissa (mantissa pfloat))
          (length (integer-length mantissa))
-         (diff (- *precision* length)))
+         (diff (cl:- *precision* length)))
     (setf (mantissa pfloat) (ash mantissa diff))
     (decf (exponent pfloat) diff))
   pfloat)
@@ -49,15 +49,15 @@
     (make-pfloat mantissa exponent)))
 
 (defun < (pfloat1 pfloat2)
-  (if (minusp (mantissa pfloat1))
-      (or (not (minusp (mantissa pfloat2)))
+  (if (cl:minusp (mantissa pfloat1))
+      (or (not (cl:minusp (mantissa pfloat2)))
           (cl:> (exponent pfloat1)
                 (exponent pfloat2))
           (and (cl:= (exponent pfloat1)
                      (exponent pfloat2))
                (cl:< (mantissa pfloat1)
                      (mantissa pfloat2))))
-      (and (not (minusp (mantissa pfloat2)))
+      (and (not (cl:minusp (mantissa pfloat2)))
            (or (cl:< (exponent pfloat1)
                      (exponent pfloat2))
                (and (cl:= (exponent pfloat1)
@@ -66,7 +66,7 @@
                           (mantissa pfloat2)))))))
 
 (defun minusp (pfloat)
-  (minusp (mantissa pfloat)))
+  (cl:minusp (mantissa pfloat)))
 
 (defun negate (pfloat)
   (make-pfloat (- (mantissa pfloat))
@@ -90,6 +90,40 @@
 
 (defun double-float-from-pfloat (pfloat)
   (dfloat (rational-from-pfloat pfloat)))
+
+(defun pfloat-from-rational (rational)
+  (let ((sign (if (cl:minusp rational) -1 1))
+        (mantissa (if (cl:minusp rational) (cl:- rational) rational))
+        exponent)
+    (let* ((numerator (numerator mantissa))
+           (numerator-length (integer-length numerator))
+           (denominator (denominator mantissa))
+           (denominator-length (integer-length denominator))
+           (difference (cl:- numerator-length denominator-length)))
+      (setf exponent difference)
+      (if (cl:minusp difference)
+          ;; Then we shift the numerator by the negative
+          ;; difference.
+          (setf numerator (ash numerator (cl:- difference)))
+          ;; Otherwise, we shift the denominator by the
+          ;; difference.
+          (setf denominator (ash denominator difference)))
+      ;; At this point either the numerator is less than the
+      ;; denominator so that the quotient is less than 1, or
+      ;; the numerator is greater than or equal to the
+      ;; denominator, so that the quotient is greater than or
+      ;; equal to 1.  In the second case, we have something
+      ;; that can be used as the mantissa of the float but in
+      ;; the first case, we must multiply the denominator by 2.
+      (when (cl:< numerator denominator)
+        (decf exponent)
+        (setf numerator (ash numerator 1)))
+      ;; Now, we shift the numerator by *PRECISION* positions to get
+      ;; something that should be an integer in the PFLOAT
+      ;; representation.
+      (setf numerator (ash numerator *precision*))
+      (decf exponent *precision*)
+      (make-pfloat (cl:* sign mantissa) exponent))))
 
 (defun pfloat-from-rational (rational)
   (let ((floatr (floatr-from-rational rational 12 200)))
