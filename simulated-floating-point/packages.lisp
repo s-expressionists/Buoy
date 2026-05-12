f20a5e8c7dd7e3602ee97747e7d29df2fb48c886
Robert Strandh <robert.strandh@gmail.com>
2026-05-12 13:55:04 +0200

    Fix typo.

--- simulated-floating-point/packages.lisp
+++ simulated-floating-point/packages.lisp
@@ -10,7 +10,7 @@
            #:rational-from-pfloat
            #:double-float-from-pfloat
            #:negate
-           #:*precison*
+           #:*precision*
            #:*one*
            . #1#))
 
