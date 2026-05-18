7d025d6cac6f7c101bd0be15758c0b9cdcaac195
Robert Strandh <robert.strandh@gmail.com>
2026-05-18 06:14:44 +0200

    Replace RATIONAL-HYPERBOLIC-COSINE by PFLOAT-HYPERBOLIC-COSINE.

--- simulated-floating-point/hyperbolic-cosine.lisp
+++ simulated-floating-point/hyperbolic-cosine.lisp
@@ -1,12 +1,9 @@
 (cl:in-package #:buoy-simulate)
 
-;;; I haven't found a way to reduce the argument, so convergence is
-;;; going to be slow for large values of the argument.
-
-(defun rational-hyperbolic-cosine (argument)
-  (loop for i from 0 by 2
-        for numerator = 1 then (* numerator argument argument)
-        for denominator = 1 then (* denominator (1- i) i)
-        for quotient = (/ numerator denominator)
-        until (< quotient #.(/ (ash 2 120)))
-        sum quotient))
+(defun pfloat-hyperbolic-cosine (pfloat)
+  (if (pf:zerop pfloat)
+      pf:*one*)
+  (let ((exp (if (pf:minusp pfloat)
+                 (pfloat-exp (pf:negate pfloat))
+                 (pfloat-exp pfloat))))
+    (pf:/ (pf:+ (pf:/ pf:*one* exp) exp) pf:*two*)))
