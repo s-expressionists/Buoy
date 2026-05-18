8861b7912d632b6b035ea7d2e08e804222baa398
Robert Strandh <robert.strandh@gmail.com>
2026-05-18 14:08:38 +0200

    Define function PFLOAT-ARCCOS.

--- simulated-floating-point/arc-cosine.lisp
+++ simulated-floating-point/arc-cosine.lisp
@@ -1,2 +1,10 @@
 (cl:in-package #:buoy-simulate)
 
+(defparameter *pfloat-pi*
+  (pf:pfloat-from-rational *pi*))
+
+(defparameter *pfloat-pi/2*
+  (pf:pfloat-from-rational (/ *pi* 2)))
+
+(defun pfloat-arccos (pfloat)
+  (pf:- *pfloat-pi/2* (pfloat-arcsine pfloat)))
