0dd8eefab04665e144d2dc4e60e5fe86086a59d4
Robert Strandh <robert.strandh@gmail.com>
2026-05-18 15:11:52 +0200

    Define function pfloat-tangent.

--- simulated-floating-point/tangent.lisp
+++ simulated-floating-point/tangent.lisp
@@ -3,3 +3,10 @@
 (defun rational-tangent (argument)
   (/ (rational-sine argument)
      (rational-cosine argument)))
+
+;;; We could do something faster here, but this should be plenty fast
+;;; for use at compile time and for generating core-math tables.
+
+(defun pfloat-tangent (pfloat)
+  (/ (pfloat-sine pfloat)
+     (pfloat-cosine pfloat)))
