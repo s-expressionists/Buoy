c48c8c7022147d738adb23805a13b1f5e2263979
Robert Strandh <robert.strandh@gmail.com>
2026-05-19 10:50:01 +0200

    Use PFLOATs to create table entries.

--- src/64/t1-table.lisp
+++ src/64/t1-table.lisp
@@ -1,14 +1,25 @@
 (cl:in-package #:buoy-core-math-64)
 
+;;; for 0 <= i < 2^6, t1[i] is a double-double approximation of
+;;; 2^(i/2^12)
+
+(defun make-t1-table-entry (i)
+  (let ((pfloat-i (pf:pfloat-from-rational i))
+        (pfloat-4096 (pf:pfloat-from-rational (expt 2 12)))
+        (pfloat-ln-2 (sim:pfloat-ln (pf:pfloat-from-rational 2)))
+        (pfloat-exponent (pf:* (pf:/ pfloat-i pfloat-4096) pfloat-ln-2))
+        (pfloat-result (sim:pfloat-exp pfloat-exponent))
+        (rational-result (pf:rational-from-pfloat pfloat-result))
+        (high (dfloat rational-result))
+        (low (dfloat (- rational-result (rational high)))))
+    (values high low)))
+
 (defparameter *t1-table*
   (make-array
    '(64 2)
    :element-type 'double-float
    :initial-contents
-   ;; Start by getting a ratioinal approximation of (expt 2 (/ 64))
-   (let* ((long-base (sim:rational-exp (* (/ 4096) (buoy-simulate:rational-ln 2))))
-          (base (sim:floatr-from-rational long-base 10 256)))
-     (loop for i from 0 below 64
-           collect (multiple-value-bind (high low)
-                       (make-t-table-element base i)
-                     (list high low))))))
+   (loop for i from 0 below 64
+         collect (multiple-value-bind (high low)
+                     (make-t1-table-entry i)
+                   (list high low)))))
