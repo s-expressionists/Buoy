3f0bbfd397a81ca94424f6d124c1772ff2738d57
Robert Strandh <robert.strandh@gmail.com>
2026-05-19 10:51:53 +0200

    Eliminate RATIONAL-LN.

--- simulated-floating-point/logarithm.lisp
+++ simulated-floating-point/logarithm.lisp
@@ -11,45 +11,6 @@
 ;;; square root of the argument until it is small enough that
 ;;; convergence is acceptable.
 
-;;; FIXME: Do this better.
-(defparameter *ln-iteration-count* 20)
-
-;;; This function is called when the argument is (1+ X) where X is a
-;;; positive value less than or equal to 1/2.
-(defun rational-ln-with-small-argument (argument count)
-  (let* ((quotient (/ (1- argument) (1+ argument)))
-         (square (* quotient quotient)))
-    (* 2 (loop for numerator = quotient then (* numerator square)
-               for denominator from 1 to count by 2
-               sum (/ numerator denominator)))))
-
-;;; This function is called when the argument is (1+ X) where X is a
-;;; positive value less than or equal to 1.
-(defun rational-ln-with-small-ish-argument (argument count)
-  (if (<= argument 3/2)
-      (rational-ln-with-small-argument argument count)
-      (* 2 (rational-ln-with-small-argument
-            (rational-square-root argument) count))))
-
-(defparameter *ln-2*
-  (* 2 (rational-ln-with-small-argument (rational-square-root 2) 20)))
-
-(defun rational-ln (argument)
-  (let* ((numerator (numerator argument))
-         (numerator-length (integer-length numerator))
-         (denominator (denominator argument))
-         (denominator-length (integer-length denominator))
-         (diff (- numerator-length denominator-length)))
-    (if (minusp diff)
-        (setf numerator (ash numerator (- diff)))
-        (setf denominator (ash denominator diff)))
-    (when (< numerator denominator)
-      (setf numerator (ash numerator 1))
-      (decf diff))
-    (+ (* diff *ln-2*)
-       (rational-ln-with-small-ish-argument
-        (/ numerator denominator) *ln-iteration-count*))))
-
 (defparameter *factors*
   (cons pf:*zero*
         (loop for i from 3 by 2 to 100
