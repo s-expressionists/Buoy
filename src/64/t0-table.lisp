2cac3edb039d36c6cc91ee7b9d21494e862e8f0a
Robert Strandh <robert.strandh@gmail.com>
2026-05-21 14:16:33 +0200

    Eliminate use of FLOATRs.

--- src/64/t0-table.lisp
+++ src/64/t0-table.lisp
@@ -10,30 +10,16 @@
 ;;;
 ;;; Oh, and in the table in core-math the first element is the low
 ;;; value and the second element is the high value.
-(defun make-t0-table-element (rational-base i)
-  (let ((pfloat-i (pf:pfloat-from-rational i))
-        (pfloat-64 (pf:pfloat-from-rational (expt 2 6)))
-        (pfloat-ln-2 (sim:pfloat-ln (pf:pfloat-from-rational 2)))
-        (pfloat-exponent (pf:* (pf:/ pfloat-i pfloat-64) pfloat-ln-2))
-        (pfloat-result (sim:pfloat-exp pfloat-exponent))
-        (rational-result (pf:rational-from-pfloat pfloat-result))
-        (high (dfloat rational-result))
-        (low (dfloat (- rational-result (rational high)))))
-    (values high low)))
+(defun make-t0-table-element (i)
+  (let ((rational-result (sim:rational-expt 2 (/ i 64))))
+    (sim:double-double-from-rational rational-result)))
 
 (defparameter *t0-table*
   (make-array
    '(64 2)
    :element-type 'double-float
    :initial-contents
-   ;; Start by getting a ratioinal approximation of (expt 2 (/ 64))
-   (let ((base (loop repeat 7
-                     for result = 2
-                       then (sim:floatr-from-rational
-                             (sim:rational-square-root result)
-                             10 256)
-                     finally (return result))))
-     (loop for i from 0 below 64
-           collect (multiple-value-bind (high low)
-                       (make-t-table-element base i)
-                     (list high low))))))
+   (loop for i from 0 below 64
+         collect (multiple-value-bind (high low)
+                       (make-t0-table-element i)
+                   (list low high)))))
