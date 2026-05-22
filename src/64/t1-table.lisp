f3a4bb2dde6ece0130e055cddd75405db11bae44
Robert Strandh <robert.strandh@gmail.com>
2026-05-22 13:17:48 +0200

    Use LET* rather than LET*.

--- src/64/t1-table.lisp
+++ src/64/t1-table.lisp
@@ -4,14 +4,14 @@
 ;;; 2^(i/2^12)
 
 (defun make-t1-table-entry (i)
-  (let ((pfloat-i (pf:pfloat-from-rational i))
-        (pfloat-4096 (pf:pfloat-from-rational (expt 2 12)))
-        (pfloat-ln-2 (sim:pfloat-ln (pf:pfloat-from-rational 2)))
-        (pfloat-exponent (pf:* (pf:/ pfloat-i pfloat-4096) pfloat-ln-2))
-        (pfloat-result (sim:pfloat-exp pfloat-exponent))
-        (rational-result (pf:rational-from-pfloat pfloat-result))
-        (high (dfloat rational-result))
-        (low (dfloat (- rational-result (rational high)))))
+  (let* ((pfloat-i (pf:pfloat-from-rational i))
+         (pfloat-4096 (pf:pfloat-from-rational (expt 2 12)))
+         (pfloat-ln-2 (sim:pfloat-ln (pf:pfloat-from-rational 2)))
+         (pfloat-exponent (pf:* (pf:/ pfloat-i pfloat-4096) pfloat-ln-2))
+         (pfloat-result (sim:pfloat-exp pfloat-exponent))
+         (rational-result (pf:rational-from-pfloat pfloat-result))
+         (high (dfloat rational-result))
+         (low (dfloat (- rational-result (rational high)))))
     (values high low)))
 
 (defparameter *t1-table*
