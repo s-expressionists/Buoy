ef9481dfce469d4a75183e01e3c46927d86db61e
Robert Strandh <robert.strandh@gmail.com>
2026-07-14 14:34:06 +0200

    Simplify code, given that X is an ordinary number.

--- src/64/acos.lisp
+++ src/64/acos.lisp
@@ -17,12 +17,13 @@
           (as-acos-refine x lb)
           lb))))
 
-(defun cr-acos (x)
+(defun acos--1<=x<=1 (x)
   (let* ((xu (f-to-i x))
          ;; Contrary to the name, AX is not the absolute value of X,
          ;; but instead the bits of X shifted one position to the
          ;; left, so that the sign bit is not taken into account.
          (ax (ash xu 1))
+         (abs-x (abs x))
          (1t 0d0)
          (z 0d0)
          (zl 0d0)
@@ -30,27 +31,16 @@
          (f0h 0d0)
          (f0l 0d0)
          (eps 0d0))
-    ;; The reason comparisons are made using the bits of X rather than
-    ;; the value of X is that X might represent infinity or NaN, and
-    ;; then direct comparisons would fail.
-    (if (> ax #x7fc0000000000000) ; |x| > 0.5
-        (let* ((off10 #.(parse-c-literal "0x1.921fb54442d18p+1"))
-               (off11 #.(parse-c-literal "0x1.1a62633145c07p-53"))
-               (sign (ash ixu -63)))
-          (setf f0h (if (zerop sign) 0d0 off10))
-          (setf f0l (if (zerop sign) 0d0 off11))
-          (when (> ax #x7fe0000000000000) ; |x| >= 1
-            (cond ((= ax #x7fe0000000000000) ; |x| = 1
-                   (return-from cr-acos (+ f0h f0l)))
-                  ((> ax #xffe0000000000000) ; Nan
-                   (return-from cr-acos (make-nan)))
-                  ((zerop sign)
-                   (error 'floating-point-overflow))
-                  (t
-                   (error 'floating-point-underflow))))
-          ;; for x>0.5 we use range reduction for double angle formula
-          ;; acos(x) = 2*asin((1-x)/2) and for x<-0.5 acos(x) = pi -
-          ;; 2*asin((1-x)/2).
+    (cond ((= x 1d0)
+           (return-from acos--1<=x<=1 0d0))
+          ((= x -1d0)
+           (let ((off0 #.(parse-c-literal "0x1.921fb54442d18p+1"))
+                 (off1 #.(parse-c-literal "0x1.1a62633145c07p-53")))
+             (return-from acos--1<=x<=1 (+ f0h f0l))))
+          ((> absx 0.5d0)
+           ;; for x>0.5 we use range reduction for double angle
+           ;; formula acos(x) = 2*asin((1-x)/2) and for x<-0.5 acos(x)
+           ;; = pi - 2*asin((1-x)/2).
           ))))
 
 (defun acos-infinity-or-nan (x)
@@ -64,4 +54,4 @@
          (error 'type-error
                 :datum x
                 :expected-type '(dfloat -1d0 1d0)))
-        (acos--1d0<=x<=1d0 x)))
+        (acos--1<=x<=1 x)))
