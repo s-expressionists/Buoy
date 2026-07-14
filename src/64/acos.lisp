1290afc180044ec8817a8fd74224b5dcd865d3b8
Robert Strandh <robert.strandh@gmail.com>
2026-07-14 15:01:01 +0200

    Add more code and fix incorrect arguments.

--- src/64/acos.lisp
+++ src/64/acos.lisp
@@ -36,12 +36,27 @@
           ((= x -1d0)
            (let ((off0 #.(parse-c-literal "0x1.921fb54442d18p+1"))
                  (off1 #.(parse-c-literal "0x1.1a62633145c07p-53")))
-             (return-from acos--1<=x<=1 (+ f0h f0l))))
+             (return-from acos--1<=x<=1 (+ off0 off1))))
           ((> absx 0.5d0)
            ;; for x>0.5 we use range reduction for double angle
            ;; formula acos(x) = 2*asin((1-x)/2) and for x<-0.5 acos(x)
            ;; = pi - 2*asin((1-x)/2).
-          ))))
+           (setf 1t (- 2d0 (* 2d0 abs-x)))
+           (setf jd (round (* 1t 32d0)))
+           (setf z (copy-sign (sqrt 1t) x))
+           (setf zl (* (fma z z (- 1t)) (* (/ -0.5d0 1t) z)))
+           (setf 1t (- (* 0.25d0 1t) (* jd #.(parse-c-literal "1.0p-7"))))
+           ;; fails with 0x1.8bp-52 for x=-0x1.3e827a2cd6d51p-1 (no FMA)
+           (setf eps (+ (* (abs (* z 1t))
+                           #.(parse-c-literal "0x1.8cp-52"))
+                        #.(parse-c-literal "0x1.0p-105"))))
+          (t
+           (setf f0h #.(parse-c-literal "0x1.921fb54442d18p+0"))
+           (setf f0l #.(parse-c-literal " 0x1.1a62633145c07p-54"))
+           (when (< abs-x #.(parse-c-literal "1.0p-15"))
+             
+
+
 
 (defun acos-infinity-or-nan (x)
   (error "Infinity or NaN supplied to ACOS"))
